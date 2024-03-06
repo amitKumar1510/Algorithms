721483e93dca9449c85de92a8cc4b05f5c9d77a6
AMIT KUMAR <123359563+amitKumar1510@users.noreply.github.com>
2024-03-06 22:55:35 +0530

    Update DijkastraAlgorithm.c

--- DijkastraAlgorithm.c
+++ DijkastraAlgorithm.c
@@ -82,3 +82,98 @@ void dijikstra(int G[MAX][MAX], int n, int startnode)
  1 0 1 0
  
  */
+// another code
+
+
+#include<stdio.h>
+#define INFINITY 9999
+#define MAX 10
+int G[MAX][MAX], i, j, n, u;
+int cost[MAX][MAX];
+
+void AdjMatrix(){
+	printf("\nEnter the no. of vertices:: ");
+	scanf("%d", &n);
+	printf("\nEnter the adjacency matrix::\n");
+	for(i=0;i < n;i++){
+		for(j=0;j < n;j++){
+			scanf("%d", &G[i][j]);
+		}
+	}
+}
+ 
+void MatrixReplaceZero(int G[MAX][MAX], int n){
+	int i,j;
+	for(i=0;i < n;i++){
+		for(j=0;j < n;j++){
+			if(G[i][j]==0){
+				cost[i][j]=INFINITY;
+			}else{
+				cost[i][j]=G[i][j];
+			}
+		}
+	}				
+}
+
+void printDistance( int startnode,int distance[],int pred[]){
+	int i,j;
+	for(i=0;i < n;i++){
+		if(i!=startnode){
+			printf("\nDistance of %d = %d", i, distance[i]);
+			printf("\nPath = %d", i);
+			j=i;
+			do{
+				j=pred[j];
+				printf(" <-%d", j);
+			}
+			while(j!=startnode);
+		}
+	}
+}
+
+void dijikstra(int startnode){
+	int distance[MAX], pred[MAX];
+	int visited[MAX], count, mindistance, nextnode, i,j;
+	
+	for(i=0;i< n;i++){
+		distance[i]=cost[startnode][i];
+		pred[i]=startnode;
+		visited[i]=0;
+	}
+	
+	distance[startnode]=0;
+	visited[startnode]=1;
+	count=1;
+	while(count < n-1){
+		mindistance=INFINITY;
+		for(i=0;i < n;i++){
+			if(distance[i] < mindistance&&!visited[i])
+			{
+				mindistance=distance[i];
+				nextnode=i;
+			}
+		}
+		visited[nextnode]=1;
+		for(i=0;i < n;i++){
+			if(!visited[i])
+				if(mindistance+cost[nextnode][i] < distance[i])
+				{
+					distance[i]=mindistance+cost[nextnode][i];
+					pred[i]=nextnode;
+				}
+			count++;
+		}
+	}
+	printDistance(startnode,distance,pred);
+}
+ 
+int main(){
+	AdjMatrix();
+	printf("\nEnter the starting node:: ");
+	scanf("%d", &u);
+	MatrixReplaceZero(G,n);
+	dijikstra(u);
+	return 0;
+}
+
+
