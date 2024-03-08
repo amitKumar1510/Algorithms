0fbde8928d61b218e6a5441278f324ab6d5a58b4
AMIT KUMAR <123359563+amitKumar1510@users.noreply.github.com>
2024-03-08 23:03:43 +0530

    Update TopologicalSorting.c

--- TopologicalSorting.c
+++ TopologicalSorting.c
@@ -1,35 +1,74 @@
 #include <stdio.h>
+#include <stdlib.h> // Include this header for using malloc
+
 # define max 10
-int arr[max][max],i,j,n;
-int visited[max],indegree[max];
+int arr[max][max], i, j, n, top = -1;
+int visited[max], indegree[max], stack[max];
 
-void AdjacencyMatrix(){
-	for(i=0;i<n;i++){
-		for(j=0;j<n;j++){
-			scanf("%d",&arr[i][j]);
-		}
-	}
+void AdjacencyMatrix() {
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
 }
 
-void Initialization(){
-	for(i=0;i<n;i++){
-		visited[i]=0;
-		indegree[i]=0;
-		for(j=0;j<n;j++){
-			indegree[i]= indegree[i]+arr[j][i];
-		}
-	}	
+void Initialization() {
+    for (i = 0; i < n; i++) {
+        visited[i] = 0;
+        indegree[i] = 0;
+        for (j = 0; j < n; j++) {
+            indegree[i] = indegree[i] + arr[j][i];
+        }
+    }
 }
 
+void push(int node) {
+    stack[++top] = node;
+}
 
+int pop() {
+    return stack[top--];
+}
 
+int isEmpty() {
+    return top == -1;
+}
 
+void TopologicalSort() {
+    int i, node;
+    
+    for (i = 0; i < n; i++) {
+        if (indegree[i] == 0 && !visited[i]) {
+            push(i);
+            visited[i] = 1;
+        }
+    }
 
-int main(){
-	printf("Enter the no of nodes in graph : ");
-	scanf("%d",&n);
-	AdjacencyMatrix();
-	Initialization();
-	
-	return 0;
+    while (!isEmpty()) {
+        node = pop();
+        printf("%d ", node);
+
+        for (i = 0; i < n; i++) {
+            if (arr[node][i] == 1 && !visited[i]) {
+                indegree[i]--;
+                if (indegree[i] == 0) {
+                    push(i);
+                    visited[i] = 1;
+                }
+            }
+        }
+    }
+}
+
+int main() {
+    printf("Enter the number of nodes in the graph: ");
+    scanf("%d", &n);
+    AdjacencyMatrix();
+    Initialization();
+
+    printf("Topological Sorting: ");
+    TopologicalSort();
+
+    return 0;
 }
